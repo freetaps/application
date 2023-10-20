e3e001eeedd0a2bf8f58cb6a4e9fbdfcecaeb3a5
Thomas Seignette <tseignette@pm.me>
2023-10-20 09:16:46 +0200

    Implement about and water bottle pages

--- src/routes/about.js
+++ src/routes/about.js
@@ -13,7 +13,23 @@ customElements.define('page-about', class extends HTMLElement {
 
 <ion-content class="ion-padding">
   <div class="ion-content-safe-area">
-    <p>TODO: page À propos</p>
+    <h1>À propos</h1>
+
+    <h2>Conditions d'utilisation</h2>
+    <p>L’utilisateur est appelé à exercer son jugement avant de consommer l’eau fournie par les points référencés sur l’application.</p>
+    <p>Les informations concernant ces points sont mises à disposition par FreeTaps et la communauté des utilisateurs, mais ne sauraient engager l’association ou ses prestataires. FreeTaps n’apporte aucune garantie quant à l’exactitude de la localisation des points d’eau, leur disponibilité ou la qualité de l’eau fournie.</p>
+    <p>Cela est particulièrement à prendre en compte si vous comptez planifier un itinéraire : FreeTaps ne peut garantir ses informations, nous vous invitons à contacter aussi les offices de tourisme ou autres habitants des régions que vous comptez traverser pour vous assurer que des possibilités de trouver de l’eau existent bien sur votre itinéraire.</p>
+
+    <h2>FreeTaps</h2>
+    <p>FreeTaps est une association fondée en 2011 dont la mission est de référencer les points d’eau potable sur une application gratuite, collaborative, partout en France, en Europe et dans le monde.</p>
+    <p>L’application s’enrichit constamment grâce aux villes et aux particuliers qui ajoutent des données : plus de 17000 points d’eau sont déjà disponibles !</p>
+    <p>Si vous êtes une collectivité ou un acteur de l’eau qui voulez référencer des points, contactez-nous !</p>
+    <p>FreeTaps a été créé par les fondateurs de Gobi.</p>
+
+    <h2>Gobi</h2>
+    <p>Gobi est une entreprise à impact dont la mission est d’accélérer le changement vers le réutilisable. Depuis 2011, Gobi propose des produits designs, éco-conçus et 100% fabriqués en France, alternatives durables aux objets à usage unique - gobelet, couverts, bouteilles jetables.</p>
+    <p>Près de 10000 organisations ont déjà adopté nos produits. Grâce à cette communauté, des millions de kg de déchets ont été évités !</p>
+    <p>Pour en savoir plus : <a href="https://www.gobilab.com/">www.gobilab.com</a>.</p>
   </div>
 </ion-content>`;
   }
--- src/routes/water-bottle.js
+++ src/routes/water-bottle.js
@@ -13,7 +13,28 @@ customElements.define('page-water-bottle', class extends HTMLElement {
 
 <ion-content class="ion-padding">
   <div class="ion-content-safe-area">
-    <p>TODO: page Bien laver sa gourde</p>
+    <h1>Bien laver sa gourde</h1>
+    <p>Comment bien laver sa gourde ?</p>
+    <p>3 conseils pour profiter de votre gourde le plus longtemps possible :</p>
+
+    <h2>Un rapide rinçage quotidien</h2>
+    <p>Important, surtout pour les amateurs de jus ou de sirop. Rincez bien, puis laissez votre gourde sécher à l’air libre pour éviter qu’elle ne garde une odeur d’humidité.</p>
+
+    <h2>Un passage au lave-vaisselle de temps à autre</h2>
+    <p>Vérifiez bien que le matériau de votre gourde supporte le lavage au lave-vaisselle - souvent, la température est de 65°C. Rapprochez-vous du fabricant pour avoir plus d’informations.</p>
+    <p>Notez que si votre gourde possède une décoration (sérigraphie, film plastique), il se peut que le lave-vaisselle dégrade progressivement cette décoration.</p>
+
+    <h2>En cas de mauvaises odeurs</h2>
+    <p>Remplissez votre gourde à moitié avec de l’eau, et ajoutez une cuillère à café de bicarbonate de soude. Fermez votre gourde, et secouez vigoureusement. Videz, puis rincez plusieurs fois, puis laissez sécher votre gourde.</p>
+
+    <div class="youtube-iframe-wrapper">
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/YrCS5cGjSaI?si=KMkbQhQC3u7oYPgK"
+        title="Vidéo tutorielle de Gobi pour bien laver sa gourde"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
   </div>
 </ion-content>`;
   }
