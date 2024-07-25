8ece9a3600f5dcb59d876ac751c8e86bfc3e750b
Thomas Seignette <tseignette@pm.me>
2024-07-25 09:24:28 +0200

    Update Ionic and Capacitor

--- src/components/water-point-record.js
+++ src/components/water-point-record.js
@@ -26,7 +26,7 @@ customElements.define('component-water-point-record', class extends HTMLElement 
       <ion-icon aria-hidden="true" name="checkmark" slot="start"></ion-icon>
       <ion-label class="water-point-use"></ion-label>
     </ion-item>
-    <ion-item class="ion-text-center" lines="none">
+    <ion-item lines="none">
       <ion-button class="water-point-report-link" color="danger" fill="clear">
         <ion-icon aria-hidden="true" name="alert-circle-outline" slot="start"></ion-icon>
         <span data-i18n="report-link"></span>
